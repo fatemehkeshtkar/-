c5b6fcabdfd70fbfe6da1d92af1674b7c60410fd
fatemehkeshtkar <fatemehkeshtkar2002@gmail.com>
2026-02-19 12:45:42 +0330

    Update session1_4.py

--- session1_4.py
+++ session1_4.py
@@ -1,15 +1,16 @@
-"فرمول"
-for i in range(start,stop,step):
-    k = ...
+"فرمول"def loop_example(start, stop, step):
+    k = 0
+    for i in range(start, stop, step):
+        k = ...      
+    return k
 
-F(n) = k/step (stop - start)
 
-
-
-"تمرین"
-for i in range(10,2*n+6,5):
-    p=k 
-    k=s 
-    j=l 
-
-F(n) = 3/5(2 n-4) = 1.2 n - 2.4
+def exercise(n):
+    p = k = j = 0  # مقداردهی اولیه
+    for i in range(10, 2 * n + 6, 5):
+        p = k
+        k = 0  
+        j = 0   
+    
+    F_n = 1.2 * n - 2.4
+    return F_n

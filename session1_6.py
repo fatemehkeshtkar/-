7f4d26b6ed34c10056e0da7c88417057f0dd4a11
fatemehkeshtkar <fatemehkeshtkar2002@gmail.com>
2026-02-19 12:48:46 +0330

    Update session1_6.py

--- session1_6.py
+++ session1_6.py
@@ -1,38 +1,35 @@
 "تکه برنامه ای بنویسید که دارای تابع پیچیدگی زمانی زیر باشد"
-F(n) = 1.3 n**2 + 6 n + 2
-      4/3 n**2 + 6 n + 2
-
-for i in range(n):
-    for j in range(0,n,3):
-        ...
-        ...
-        ...
-        ...  
-    ...
-    ...
-    ...
-    ...
-    ...
-    ...    
-...
-...
+def loop_pattern1(n):
+    for i in range(n):
+        for j in range(0, n, 3):
+            # هر نقطه چین یک دستور است
+            a = i + j
+            b = i - j
+            c = i * j
+            d = j - i
+        e = i**2
+        f = i + 2
+        g = i - 2
+        h = 2 * i
+        i_val = i // 2
+        j_val = i % 2
 
 
-#هر کدوم از نقطه چین ها یک دستور هستند
+def loop_pattern2(n):
+    for i in range(0, n, 3):
+        for j in range(-1, n, 3):
+            a = i + j
+            b = i - j
+        c = i**2
+        d = i * 2
+        e = i + 5
+        f = i - 5
+
+# محاسبه تقریبی F(n) = 2/9 * n**2 + 14/9 * n
+def F_n_student(n):
+    return 2/9 * n**2 + 14/9 * n
 
 
-#for i in range(0,n,3):
-#    for i in range(-1,n,3):
-#         ...
-#         ...
-#    ...
-#    ...
-#    ...
-#    ...
-
-#F(n) = 1/3 (n-0) * (n+1) * 2 + 4/3 n 
-#     = 2/9 n**2 + 14/9 n       
-# ***مثال از یک دانشجو بود***  
-      
-            
-              
+# محاسبه F(n) = 1.3*n**2 + 6*n + 2 (نمونه دیگر)
+def F_n_example(n):
+    return 1.3 * n**2 + 6 * n + 2

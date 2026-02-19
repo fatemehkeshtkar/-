b48a9d228a970a4c7a0ca42aec028931ad3b8951
fatemehkeshtkar <fatemehkeshtkar2002@gmail.com>
2026-02-19 12:37:35 +0330

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,8 +1,12 @@
 "تابع پیچیدگی زمانی الگوریتم زیر را محاسبه کنید"
-a = 5
-for i in range (n):
-    p=t
-    t=l+6
-l = 2 + l
+def compute(n):
+    a = 5
+    l = 0
+    t = 0
 
-F(n) = 2 n + 2
+    for i in range(n):
+        p = t
+        t = l + 6
+        l += 2
+
+    return a, l, t

5b911dbaabeef0a85515dd0c51b966dfdfa7052f
fatemehkeshtkar <fatemehkeshtkar2002@gmail.com>
2026-02-19 12:47:53 +0330

    Update session1_5.py

--- session1_5.py
+++ session1_5.py
@@ -1,14 +1,13 @@
-"کوییز"
-for i in range(n):
-    for j in range(i,n,1):
-        k=p 
-        print(k)
-        print(p)
+"کوییز سرکلاس"
+def nested_loop_example(n):
+    p = 0  # مقدار اولیه p
+    for i in range(n):
+        for j in range(i, n):
+            k = p
+            print(k)
+            print(p)
 
-n+(n-1)+(n-2)+...+1
-1 + 2 + 3 + ...+ n 
-(n+1)+(n+1)+(n+1)+...+(n+1)
-= n (n+1) / 2
 
-F(n) = 3/2(n)(n+1)
-     = 1.5 n**2 + 1.5 n 
+def F_n_formula(n):
+    # محاسبه F(n) = 3/2 * n * (n + 1)
+    return 1.5 * n**2 + 1.5 * n
